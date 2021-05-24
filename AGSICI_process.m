acc7cb8931b265640d4ac163e5f973328027b7a7
Dominika Sulcova <dominika.sulcova@uclouvain.be>
2021-05-24 13:26:42 +0200

    fcn: get_position

--- AGSICI_process.m
+++ AGSICI_process.m
@@ -823,4 +823,13 @@ if xCursor >= minx && xCursor <= maxx && yCursor >= miny && yCursor <= maxy
     hold on
 end
 end
+function pos_x = get_position(axesHandles)
+% wait until the mouse is clicked
+w = waitforbuttonpress;
+
+% get the position of the mouse
+CP = get(axesHandles(1), 'CurrentPoint');
+pos_x = CP(1,1);
+
+end
 end
