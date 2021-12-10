b74625023e78f8b3c2557732ab7fd61c39df4974
Dominika Sulcova <dominika.sulcova@uclouvain.be>
2021-12-10 12:29:00 +0100

    P2 processing

--- AGSICI_P2_process.m
+++ AGSICI_P2_process.m
@@ -37,12 +37,12 @@ clear p i
 answer = questdlg('Do you want to choose a new colour scheme?', 'Colour scheme', 'YES', 'NO', 'NO'); 
 switch answer
     case 'YES'
-        for a = 1:length(intensity)+1
+        for a = 1:length(conditions)
            colours(a, :) = uisetcolor; 
         end
     case 'NO'
-        if exist('colours.mat') > 0
-            load('colours.mat')
+        if exist('P2_colours.mat') > 0
+            load('P2_colours.mat')
         else
             disp('No colour scheme found in this directory!')    
         end
@@ -116,12 +116,12 @@ clear s c
 
 %% 3) TEP amplitude extraction
 % ----- decide output parameters -----
-AGSICI_P2_default.peak = {'P25' 'N75' 'N100' 'P180'};                               % peaks of interest
-AGSICI_P2_default.center = [0.025 0.075 0.1 0.2];                                           % default starting latencies
-AGSICI_P2_default.span = [0.015 0.03 0.06 0.06];                                             % span of the extraction window
-AGSICI_P2_default.eoi = {{'C1' 'C3' 'CP5'} {'Cz'} {'Fz' 'FC2' 'F4'} {'Cz'}};        % electrodes of interest
-percent = 20;                                                                       % % of timepoints included in the mean amplitude calculation
-map_lims = [-4 4];                                                                  % y limits for topoplots 
+AGSICI_P2_default.peak = {'N15' 'P25' 'N40' 'N75' 'N100' 'P180'};                                           % peaks of interest
+AGSICI_P2_default.center = [0.015 0.025 0.04 0.075 0.1 0.2];                                                % default starting latencies
+AGSICI_P2_default.span = [0.01 0.015 0.015 0.03 0.06 0.06];                                                            % span of the extraction window
+AGSICI_P2_default.eoi = {{'target'} {'C1' 'C3' 'CP5'} {'target'} {'Cz'} {'Fz' 'FC2' 'F4'} {'Cz'}};          % electrodes of interest
+percent = 20;                                                                                               % % of timepoints included in the mean amplitude calculation
+map_lims = [-4 4];                                                                                          % y limits for topoplots 
 % ------------------------------------
 % set colours for visualisation - figure 1
 col_fig1 = [0    0.4471    0.7412;
@@ -136,13 +136,12 @@ col_fig1 = [0    0.4471    0.7412;
 col_fig2 = [1.0000    0.4118    0.1608;
     0.8902    0.0549    0.0549;
     0.6353    0.0784    0.1843;
-    0.4941    0.1843    0.5569];
+    0.4941    0.1843    0.5569;
+    0.2941    0.2510    0.8706;
+    0.0745    0.6235    1.0000];
 
 % loop through subjects
-for s = 1:length(subject)
-    % set axis counter
-    axis_counter = 1;
-
+for s = 2:length(subject)
     % setup name  
     figure_title = sprintf('Subject n. %d', subject(s)); 
     
@@ -150,8 +149,8 @@ for s = 1:length(subject)
     if figure_counter < 3
         figure_counter = 3;
     end
-    fig = figure(figure_counter);
     axis_counter = 1;
+    fig = figure(figure_counter);
     
     % adjust figure size
     set(gcf,'units','normalized','outerposition',[0 0 1 1])
@@ -353,7 +352,7 @@ for s = 1:length(subject)
 
         % add topoplots
         topoplot_titles = conditions;
-        for a = 1:4
+        for a = 1:length(AGSICI_P2_default.peak)
             subplot(n_row, n_col, 3 + a + n_col*(axis_counter - 1))
             topo_plot(header, data_topoplot(a, :, :, :, :, :), lat_peak(a), time_window(1), map_lims);   
             if axis_counter == 1
@@ -390,13 +389,242 @@ for s = 1:length(subject)
     % update the figure counter
     figure_counter = figure_counter + 1;  
 end
-clear fig_1 choose_header choose_map_lims choose_span choose_x choose_x1 choose_x2 l a choose_fig pos_x diff...
-    choose data choose_center choose_axesHandles answer
-clear col_fig1 col_fig2 s c figure_title fig axis_counter k polarity e i data_visual data_topoplot a subj figure_name
+clear finish fig_1 choose_header choose_map_lims choose_span choose_x choose_x1 choose_x2 l a choose_fig pos_x diff...
+    choose data choose_center choose_axesHandles answer col_fig1 col_fig2 s c figure_title fig axis_counter k choose_data choose_figure_name...
+    polarity e i data_visual data_topoplot a subj figure_name n_col n_row eoi lat_peak span center yl yl_sgtitle y_mean y_max p 
 
 % append new variables to the general MATLAB file
 save(output_file, 'AGSICI_P2_default', '-append');
 clear percent map_lims
+
+%% 4) TEP amplitude - visualization
+% ----- decide output parameters -----
+index = logical([0 1 0 0 1 1 1;
+    1 1 1 1 1 1 0;
+    1 1 1 1 1 1 1;
+    0 1 1 1 1 1 1;
+    1 1 1 1 1 1 1;
+    1 1 1 1 1 1 1]);                                                                                        % y limits for topoplots 
+% ------------------------------------
+% plot mean spTMS amplitudes
+for p = 1:size(AGSICI_P2_TEP.amplitude_peak, 3)     
+    %  get mean data
+    data_visual = []; sem_visual = [];
+    for c = 1:4
+        data_visual(c) = mean(AGSICI_P2_TEP.amplitude_peak(index(p, :), c, p));
+        sem_visual(c) = std(AGSICI_P2_TEP.amplitude_peak(index(p, :), c, p))/sqrt(length(subject(index(p, :))));
+    end
+
+    % launch the figure
+    fig = figure(figure_counter);
+    hold on
+    
+    % plot the amplitudes
+    for c = 1:4
+        bar(c, data_visual(c), 'EdgeColor', 'none', 'FaceColor', colours(c, :))
+    end
+    
+    % errorbars
+    errorbar(1:4, data_visual, sem_visual, sem_visual, 'k', 'linestyle', 'none');  
+    
+    % add parameters
+    title(sprintf('peak amplitude: %s', AGSICI_P2_default.peak{p}))
+    ylabel('amplitude (\muV)')
+    set(gca, 'FontSize', 14)
+    
+    % name and save figure
+    figure_name = ['AGSICI_TEP_'  AGSICI_P2_default.peak{p}];
+    savefig([folder_figures '\' figure_name '.fig'])
+    saveas(fig, [folder_figures '\' figure_name '.png'])
+    
+    % upgrade counter
+    figure_counter = figure_counter + 1;
+end
+clear p c data_visual sem_visual figure_name
+
+%% 5) SICI: TEPs
+% ----- decide output parameters -----
+electrode = {'target' 'Cz'};
+linestyle = {'-' ':'};
+% ------------------------------------
+% calculate individual SICI
+AGSICI_P2_SICI = struct;
+for s = 1:length(subject)
+    for c = 5:7
+        for e = 1:size(AGSICI_P2_data, 3)
+            % calculate GFP (exclude target channel)
+            AGSICI_P2_SICI.individual(s, c-4, e, :) = squeeze(AGSICI_P2_data(s, c, e, :)) - squeeze(AGSICI_P2_data(s, 1, e, :));  
+        end
+    end
+end
+clear s c e
+
+% calculate mean SICI
+for c = 1:3
+    for e = 1:size(AGSICI_P2_data, 3)
+        for i = 1:size(AGSICI_P2_data, 4)
+            AGSICI_P2_SICI.mean(c, e, i) = mean(squeeze(AGSICI_P2_SICI.individual(:, c, e, i)));
+            AGSICI_P2_SICI.CI(c, e, i) = (std(squeeze(AGSICI_P2_SICI.individual(:, c, e, i)))/sqrt(length(subject))) * z;
+        end
+    end  
+end 
+clear c e i 
+
+% plot mean SICI 
+for c = 1:3
+    % prepare data
+    data_visual = squeeze(AGSICI_P2_SICI.mean(c, :, :));
+    
+    % launch the figure
+    fig = figure(figure_counter);
+    hold on
+
+    % set limits of the figure
+    yl = [-2.5 2.5];
+
+    % shade interpolated interval
+    plot(x, data_visual(1, :), 'b:', 'LineWidth', 0.5);
+    rectangle('Position', [-0.005, yl(1), 0.015, yl(2) - yl(1)], 'FaceColor', [0.75 0.75 0.75], 'EdgeColor', 'none')
+    
+    % plot all channels
+    for e = 1:size(data_visual, 1)
+        plot(x, data_visual(e, :), 'Color', colours(c, :))
+    end
+    
+    % plot electrode(s) of interest
+    for e = 1:length(electrode)
+        e_n = find(strcmp(labels, electrode{e}));
+        e_p(e) = plot(x, data_visual(e_n, :), 'Color', [0 0 0], 'LineWidth', 2.5, 'LineStyle', linestyle{e})
+    end
+
+    % mark TMS stimulus and zerol line
+    line([0, 0], yl, 'LineStyle', '--', 'Color', [0, 0, 0], 'LineWidth', 2.5)
+    line(time_window, [0, 0], 'LineStyle', ':', 'Color', [0, 0, 0], 'LineWidth', 1.5)
+
+    % add other parameters
+    title(sprintf('SICI in AG: %s', conditions{4+c}))
+    ylabel('amplitude (\muV)')
+    set(gca, 'FontSize', 14)
+    xlim(time_window)
+    ylim(yl)
+    
+    % add legend
+    lgd = legend(e_p, electrode, 'Location', 'southeast');
+    lgd.FontSize = 14;
+    hold off
+ 
+    % change figure size
+    fig.Position = [250 250 600 350];
+
+    % name and save figure
+    figure_name = ['AGSICI_TEP_SICI_'  conditions{4+c}];
+    savefig([folder_figures '\' figure_name '.fig'])
+    saveas(fig, [folder_figures '\' figure_name '.png'])
+
+    % update figure counter
+    figure_counter = figure_counter + 1 ;
+end
+clear c data_visual fig yl lgd figure_name 
+
+% save SICI datasets for letswave
+for c = 1:3
+    % create data
+    for s = 1:length(subject)
+        data(s, :, 1, 1, 1, :) = squeeze(AGSICI_P2_SICI.individual(s, c, :, :));
+    end
+
+    % create header
+    header.name = ['merged SICI ' conditions{4+c}];
+    header.datasize = size(data);
+    header.xstart = time_window(1);
+
+    % save
+    save([header.name '.mat'], 'data')
+    save([header.name '.lw6'], 'header')
+end
+clear c s
+
+% calculate GFP of SICI
+for s = 1:length(subject)
+    for c = 1:3
+        % calculate GFP (exclude target channel)
+        AGSICI_P2_SICI.GFP(s, c, :) = std(squeeze(AGSICI_P2_SICI.individual(s, c, 1:32, :)), 1);  
+    end
+end
+clear s c 
+clear electrode linestyle
+
+% append to the general MATLAB file
+save(output_file, 'AGSICI_P2_SICI', '-append');
+
+%% 6) SICI: change in the amplitude
+% calculate individual change for peak amplitude of each TEP peak 
+for p = 1:length(AGSICI_P2_default.peak) 
+    for s = subject(index(p, :))
+        for c = 1:3        
+            AGSICI_P2_SICI.peak_change(s, c, p) = AGSICI_P2_TEP.amplitude_peak(s, 4+c, p) - AGSICI_P2_TEP.amplitude_peak(s, 1, p);
+        end
+    end
+end
+clear s c p
+
+% append to the general MATLAB file
+save(output_file, 'AGSICI_P2_SICI', '-append');
+
+% get data for visualization
+data_visual = []; sem_visual = [];
+for p = 1:length(AGSICI_P2_default.peak)
+    for c = 1:3
+        % identify peak polarity, get the data
+        if AGSICI_P2_default.peak{p}(1) == 'P'
+            polarity = 1;
+        else
+            polarity = -1;
+        end
+        
+        %
+        data_visual(p, c) = mean(AGSICI_P2_SICI.peak_change(subject(index(p, :)), c, p)) * polarity;
+        sem_visual(p, c) = std(AGSICI_P2_SICI.peak_change(subject(index(p, :)), c, p))/sqrt(length(subject(index(p, :))));
+    end
+end
+clear p c polarity     
+
+% launch the figure
+fig = figure(figure_counter);
+hold on
+
+% plot errorbars
+ngroups = size(data_visual, 1);
+nbars = size(data_visual, 2);
+groupwidth = min(0.8, nbars/(nbars + 1.5));
+for i = 1:nbars
+    x_bar = (1:ngroups) - groupwidth/2 + (2*i-1) * groupwidth / (2*nbars);
+    errorbar(x_bar, data_visual(:, i), sem_visual(:, i), sem_visual(:, i), 'k', 'linestyle', 'none');
+end 
+
+% plot the data
+barplot = bar(data_visual, 'EdgeColor', 'none');
+col = colours([2:4], :);
+for b = 1:size(data_visual, 2)
+    barplot(b).FaceColor = col(b, :);
+end
+
+% set other parameters
+title('AG SICI: change in TEP peak amplitude')
+ylabel('amplitude (\muV \pmSEM)');
+xlabel('TEP component')
+set(gca, 'xtick', 1:6, 'xticklabel', AGSICI_P2_default.peak)
+set(gca, 'Fontsize', 14)
+legend(barplot, intensity, 'Location', 'southeast', 'fontsize', 14)
+
+% name and save figure
+figure_name = 'SICI_amplitude';
+savefig([folder_figures '\' figure_name '.fig'])
+saveas(fig, [folder_figures '\' figure_name '.png'])
+
+% update figure counter
+figure_counter = figure_counter + 1;
+clear data_visual sem_visual fig i nbars ngroups x_bar b barplot col figure_name
 
 %% functions
 function peak_x = gfp_plot(x, y, time_window, xstep, labeled, varargin)
